59c357fdf6ac89d66f2a0fc33f84e9e2656e341b
Khadija Rafaqat <khadijarafaqat2@gmail.com>
2026-02-02 23:16:21 +0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import logo from '../Assets/logo.svg'
+import srch from '../Assets/search.svg'
+import cart from '../Assets/cart.svg'
+
+const Navbar = () => {
+  return (
+   <header>
+    <nav>
+        <img src={logo} alt="Apple logo" />
+
+        <ul>
+          {[
+            {label:'Store'},
+            {label:'Mac'},
+            {label:'iPhone'},
+            {label:'Watch'},
+            {label: 'Vision'},
+            {label:'AirPods'},
+          ].map(({label})=> (
+            <li key={label}>
+              <a href={label}>{label}</a>
+            </li>
+          ))}
+        </ul>
+          <div className='flex-center gap-3'>
+            <button>
+              <img src={srch} alt="Search" />
+            </button>
+
+            <button>
+              <img src={cart} alt="Cart" />
+            </button>
+          </div>
+
+
+    </nav>
+   </header>
+  )
+}
+
+export default Navbar
